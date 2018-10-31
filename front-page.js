c9e5b81e7043d9e9b295de0ddf1b86ec1208cb02
Andrew Weaver <weevz@uw.edu>
2018-10-31 16:02:40 -0700

    js updates

--- front-page.js
+++ front-page.js
@@ -1,14 +1,18 @@
 function imageSearch() {
-    var page = document.getElementById('ImageSearchTerm').value
-    window.open("http://cdm16866.contentdm.oclc.org/digital/search/searchterm/" + ImageSearchTerm.value + "!image" + "/field/all!type/mode/all!all/conn/and!and/cosuppress/","_self");
+    var input = document.getElementById('ImageSearchTerm').value
+    window.open("http://cdm16866.contentdm.oclc.org/digital/search/searchterm/" + input + "!image" + "/field/all!type/mode/all!all/conn/and!and/cosuppress/","_self");
 }
 
 function allSearch() {
-    var page = document.getElementById('ImageSearchTerm').value
     window.open("http://cdm16866.contentdm.oclc.org/digital/search");
 }
 
 function crbehaSearch() {
-    var page = document.getElementById('SearchTerm').value
-    window.open("http://cdm16866.contentdm.oclc.org/digital/search/collection/5985!cchm!wsuvan1!imls_2!imls_3!cvoralhist!methhist/searchterm/" + SearchTerm.value + "/field/all/mode/all/conn/and/cosuppress/","_self");
+    var input = document.getElementById('SearchTerm').value
+    window.open("http://cdm16866.contentdm.oclc.org/digital/search/collection/5985!cchm!wsuvan1!imls_2!imls_3!cvoralhist!methhist/searchterm/" + input + "/field/all/mode/all/conn/and/cosuppress/","_self");
+}
+
+function SearchDropDownSociety() {
+    var input = document.getElementById('SearchPathSociety').value
+    window.open("http://cdm16866.contentdm.oclc.org/digital/search/collection/" + input + "/field/all/mode/all/conn/and/cosuppress/","_self");
 }
