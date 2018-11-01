7d1e244e4c2ddbc9f58186eb58e57f0f53a9d932
Weaver <weevz@uw.edu>
2018-11-01 15:54:29 -0700

    update js

--- front-page.js
+++ front-page.js
@@ -12,6 +12,12 @@ function crbehaSearch() {
     window.open("http://cdm16866.contentdm.oclc.org/digital/search/collection/5985!cchm!wsuvan1!imls_2!imls_3!cvoralhist!methhist/searchterm/" + input + "/field/all/mode/all/conn/and/cosuppress/","_self");
 }
 
+function crbehaSearch2() {
+    var input = document.getElementById('SearchTerm2').value
+    window.open("http://cdm16866.contentdm.oclc.org/digital/search/collection/5985!cchm!wsuvan1!imls_2!imls_3!cvoralhist!methhist/searchterm/" + input + "/field/all/mode/all/conn/and/cosuppress/","_self");
+}
+
+
 function SearchDropDownSociety() {
     var input = document.getElementById('SearchPathSociety').value
     window.open("http://cdm16866.contentdm.oclc.org/digital/search/collection/" + input + "/mode/all/conn/and/cosuppress/","_self");
