cb492200cab5678998f7afbd5be12881759f7d2c
Weaver <weevz@uw.edu>
2019-01-31 13:19:12 -0800

    js search updates

--- main-custom.js
+++ main-custom.js
@@ -45,22 +45,64 @@ function SearchDropDownDate() {
 
 // functions for custom searches in pages (single collection)
 
-function SingleSearchType(){
+function SingleSearchKeyword(){
     var collectionToSearch = window.location.href.split('/collection/')[1].split('/')[0];
-    var input = document.getElementById('SearchPathType').value
-    window.open("/digital/collection/" + collectionToSearch + "/search/searchterm/" + input + "/field/type/mode/all/conn/and/order/nosort/ad/asc","_self");
+    var input = document.getElementById('SingleSearchKeywordInput').value
+    window.open("/digital/collection/" + collectionToSearch + "/search/searchterm/" + input + "/field/all/mode/all/conn/and/order/nosort/ad/asc","_self");
 }
 
-function SingleSearchDate(){
+function SingleSearchType(){
     var collectionToSearch = window.location.href.split('/collection/')[1].split('/')[0];
-    var input = document.getElementById('SearchPathDate').value
-    window.open("/digital/collection/" + collectionToSearch + "/search/searchterm/" + input + "/field/all/mode/all/conn/and/order/nosort/ad/asc","_self");
+    var input = document.getElementById('SingleSearchTypeInput').value
+    window.open("/digital/collection/" + collectionToSearch + "/search/searchterm/" + input + "/field/type/mode/all/conn/and/order/nosort/ad/asc","_self");
 }
 
 function SingleSearchSubject(){
     var collectionToSearch = window.location.href.split('/collection/')[1].split('/')[0];
-    var input = document.getElementById('SearchPathSubject').value
+    var input = document.getElementById('SingleSearchSubjectInput').value
     window.open("/digital/collection/" + collectionToSearch + "/search/searchterm/" + input + "/field/subject/mode/all/conn/and/order/nosort/ad/asc","_self");
+}
+
+function SingleSearchDate(){
+    var collectionToSearch = window.location.href.split('/collection/')[1].split('/')[0];
+    var rawInput = document.getElementById('SingleSearchDateInput').value
+    if (rawInput == '1860-1869'){
+        var parsedInput = '1860 1861 1862 1863 1864 1865 1866 1867 1868 1869'
+    } else if (rawInput == '1870-1879'){
+         var parsedInput = '1870 1871 1872 1873 1874 1875 1876 1877 1878 1879'
+    } else if (rawInput == '1880-1889'){
+         var parsedInput = '1880 1881 1882 1883 1884 1885 1886 1887 1888 1889'
+    } else if (rawInput == '1890-1899'){
+         var parsedInput = '1890 1891 1892 1893 1894 1895 1896 1897 1898 1899'
+    } else if (rawInput == '1900-1909'){
+         var parsedInput = '1900 1901 1902 1903 1904 1905 1906 1907 1908 1909'
+    } else if (rawInput == '1910-1919'){
+         var parsedInput = '1910 1911 1912 1913 1914 1915 1916 1917 1918 1919'
+    } else if (rawInput == '1920-1929'){
+         var parsedInput = '1920 1921 1922 1923 1924 1925 1926 1927 1928 1929'
+    } else if (rawInput == '1930-1939'){
+         var parsedInput = '1930 1931 1932 1933 1934 1935 1936 1937 1938 1939'
+    } else if (rawInput == '1940-1949'){
+         var parsedInput = '1940 1941 1942 1943 1944 1945 1946 1947 1948 1949'
+    } else if (rawInput == '1950-1959'){
+         var parsedInput = '1950 1951 1952 1953 1954 1955 1956 1957 1958 1959'
+    } else if (rawInput == '1960-1969'){
+         var parsedInput = '1960 1961 1962 1963 1964 1965 1966 1967 1968 1969'
+    } else if (rawInput == '1970-1979'){
+         var parsedInput = '1970 1971 1972 1973 1974 1975 1976 1977 1978 1979'
+    } else if (rawInput == '1980-1989'){
+         var parsedInput = '1870 1871 1872 1873 1874 1875 1876 1877 1878 1879'
+    } else if (rawInput == '1990-1999'){
+         var parsedInput = '1980 1981 1982 1983 1984 1985 1986 1987 1988 1989'
+    } else if (rawInput == '2000-2009'){
+         var parsedInput = '2000 2001 2002 2003 2004 2005 2006 2007 2008 2009'
+    } else if (rawInput == '2010-2019'){
+         var parsedInput = '2010 2011 2012 2013 2014 2015 2016 2017 2018 2019'
+    } else if (rawInput == '2020-2029'){
+         var parsedInput = '2020 2021 2022 2023 2024 2025 2026 2027 2028 2029'
+    } 
+
+    window.open("/digital/collection/" + collectionToSearch + "/search/searchterm/" + parsedInput + "/field/date/mode/any/conn/and/order/nosort/ad/asc","_self");
 }
 
 // functions for limiting advanced search collections
